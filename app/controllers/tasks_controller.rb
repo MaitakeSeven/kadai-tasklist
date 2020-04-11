abf8449bea784cb1fbc7fac70fc4c4927cc7deea
Maitake_Seven <daisukevita108@gmail.com>
2020-04-11 08:25:08 +0000

    heroku v2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,6 +7,10 @@ class TasksController < ApplicationController
 
     def show
         @tasks = Task.find(params[:id])
+        if @tasks.user_id == current_user.id
+        else
+        redirect_to root_path      
+        end
     end
 
     def new
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,9 +1,11 @@
 module SessionsHelper
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
+    #ログイン中のアカウントIDを代入 
   end
 
   def logged_in?
     !!current_user
   end
+  
 end
